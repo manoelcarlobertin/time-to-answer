f85585dd79689d5c436dcc8d68a9db1bfb707a81
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-04-11 22:55:07 -0300

    criado os 2 CSS em assets

--- app/controllers/admins_backoffice/welcome_controller.rb
+++ app/controllers/admins_backoffice/welcome_controller.rb
@@ -1,4 +1,13 @@
-class AdminsBackoffice::WelcomeController < ApplicationController
-  def index
+module AdminsBackoffice
+  class WelcomeController < ApplicationController
+    layout "admins_backoffice"
+    # before_action :authenticate_user!
+    # before_action :check_admin
+
+    private
+
+    def check_admin
+      redirect_to root_path, alert: "Você não tem permissão para acessar essa área." unless current_user.admin?
+    end
   end
 end
--- app/controllers/users_backoffice/welcome_controller.rb
+++ app/controllers/users_backoffice/welcome_controller.rb
@@ -1,4 +1,13 @@
-class UsersBackoffice::WelcomeController < ApplicationController
-  def index
+module UsersBackoffice
+  class WelcomeController < ApplicationController
+    layout "users_backoffice"
+    # before_action :authenticate_user!
+    # before_action :check_user
+
+    private
+
+    def check_user
+      redirect_to root_path, alert: "Você não tem permissão para acessar essa área." unless current_user.user?
+    end
   end
 end
