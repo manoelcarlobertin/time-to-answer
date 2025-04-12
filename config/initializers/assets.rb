77a016f86a409d24b0f6e19f066b1ccf0db8c8ec
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-04-12 16:52:48 -0300

    sem erros

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,19 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
 Rails.application.config.assets.precompile << "bootstrap.bundle.min.js"
+
+# /app/assets
+Rails.application.config.assets.precompile += %w[ admins_backoffice.js admins_backoffice.css
+                                                  users_backoffice.js users_backoffice.css
+                                                  admin_devise.js admin_devise.css
+                                                  user_devise.js user_devise.css
+                                                  site.js site.css ]
+
+# /lib/assets
+Rails.application.config.assets.precompile += %w[ sb-admin-2.js sb-admin-2.css
+                                                  custom.js custom.css
+                                                  img.jpg
+                                                  surface-fix.js surface-fix.css
+                                                  navbar.css ]
+# /vendor/assets
+Rails.application.config.assets.precompile += %w[ jquery-2.2.3/dist/jquery.js ]
