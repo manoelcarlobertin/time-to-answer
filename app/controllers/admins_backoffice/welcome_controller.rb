5dde94e2f08fc02cfedebbb75fcb30c0403e90e8
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-04-12 14:37:06 -0300

    ao invés do link era um button pra logout do admin

--- app/controllers/admins_backoffice/welcome_controller.rb
+++ app/controllers/admins_backoffice/welcome_controller.rb
@@ -2,5 +2,12 @@ module AdminsBackoffice
   class WelcomeController < ApplicationController
     before_action :authenticate_admin!
     layout "admins_backoffice"
+
+    def index
+      @total_users = User.count
+      @total_admins = Admin.count
+      # @total_questions = Question.count
+      # @total_subjects = Subject.count
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,18 @@ Rails.application.routes.draw do
     get "welcome/index"
   end
   namespace :admins_backoffice do
-    get "welcome/index"
+    get "welcome/index" # Dashboard
+    resources :admins   # Administradores
   end
   namespace :users_backoffice do
     get "welcome/index"
   end
+
   devise_for :admins
   devise_for :users
 
   get "home", to: "site/welcome#index"
+  get "backoffice", to: "admins_backoffice/welcome#index"
   # Defines the root path route ("/")
   root to: "site/welcome#index"
 end
